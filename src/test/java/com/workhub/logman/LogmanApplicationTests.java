c6e8a8dc610ca11330638ef933a25079c2b72298
warferton <warferton45@gmail.com>
2021-11-22 02:33:19 +0500

    Init Repo

---
+++ src/test/java/com/workhub/logman/LogmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workhub.logman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
